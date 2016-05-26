aa460c4848392f761cece567dd9dca29eb997bb7
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-26 10:54:13 -0500

    Set current user

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import config from 'peepchat/config/environment';
+import fetch from 'ember-network/fetch';
 
 export default Ember.Route.extend({
   session: Ember.inject.service(),
@@ -7,5 +9,18 @@ export default Ember.Route.extend({
     if(!this.get('session.isAuthenticated')) {
       this.transitionTo('auth.login');
     }
+  },
+
+  afterModel() {
+    return fetch(`${config.DS.host}/${config.DS.namespace}/user/current`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.get('session.data.authenticated.access_token')}`
+      }
+    }).then(raw => raw.json())
+    .then((data) => {
+      const currentUser = this.store.push(data);
+      this.set('session.currentUser', currentUser);
+    });
   }
 });
